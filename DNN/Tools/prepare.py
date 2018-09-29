24f37aedb67df992e6d0e34bb2cab8918bfd2fda
Cyfroni <karolplg@gmail.com>
2018-09-30 00:26:07 +0200

    script to count lines amount and their length

---
+++ DNN/Tools/prepare.py
@@ -0,0 +1,24 @@
+from os import listdir
+from os.path import isfile
+
+path = '../'
+
+onlyfiles = [f for f in listdir(path) if isfile(path + f)]
+for file in onlyfiles:
+    with open(path + file, 'r+') as f:
+        content = f.read()
+
+        data = [line.split(',') for line in content.split('\n')]
+        first_row = data[0]
+        lines_amount = int(first_row[0]) + 1
+        lines_length = int(first_row[1])
+
+        if lines_amount == len(data) and lines_length == len(data[-1]) and len(first_row) == 2:
+            continue
+        print(file, ': correcting')
+
+        lines_amount = len(data)
+        lines_length = len(data[-1])
+        f.seek(0, 0)
+        f.write(str(lines_amount) + ',' + str(lines_length) + '\n')
+        f.write(content)
