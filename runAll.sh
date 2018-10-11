dd7f91919d7c6b450a119a3acc5e8e833d521fe0
Cyfroni <karolplg@gmail.com>
2018-10-11 19:20:52 +0200

    add scripts

--- runAll.sh
+++ runAll.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 git submodule update --remote
 source chapel-1.18.0/util/setchplenv.bash
+
 cd RoutingTable
 chmod 755 run.sh
-./run.sh graph50.in #*.in
+./run.sh
+cd ..
+
+cd DNN
+chmod 755 run.sh
+./run.sh
+cd ..
+
+cd CenPowell
+chmod 755 run.sh
+./run.sh
+cd ..
