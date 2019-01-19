4a9a473c448d2eaa980be50fd19280042c55935d
Cyfroni <karolplg@gmail.com>
2019-01-19 22:11:50 +0100

    prepare dnn for cancer

--- Tools/prepare.py
+++ Tools/prepare.py
@@ -13,18 +13,21 @@ for file in onlyfiles:
         data = [line.split(',') for line in content.splitlines()]
         first_row = data[0]
 		
-        lines_to_ignore = first_row[0].split(';')
+        lines = first_row[0].split(';')
         #print(lines_to_ignore)
-        if len(lines_to_ignore) == 1:
+        if len(lines) == 1:
             continue
         print(file + ': correcting')
-        print('ignoring: ' + str(lines_to_ignore))
-
-        lines_to_ignore = [int(elem) for elem in lines_to_ignore if elem != '']
+        print('lines: ' + str(lines))
+	
+        category_column = int(lines[-1])
+        del lines[-1]
+        lines_to_ignore = [int(elem) for elem in lines if elem != '']
         lines_to_ignore.sort()
         lines_to_ignore.reverse()
         del data[0]
-		
+	
+        categories = {line[category_column] for line in data}
         for col_number in lines_to_ignore:
             for row in data:
                 del row[col_number]
@@ -32,7 +35,6 @@ for file in onlyfiles:
 		
         lines_amount = len(data)
         lines_length = len(data[-1])
-        categories = {line[0] for line in data}
         f.seek(0, 0)
         f.truncate(0)
         f.write(str(lines_amount) + ',' + str(lines_length) + ',' + str(len(categories)) + '\n')
