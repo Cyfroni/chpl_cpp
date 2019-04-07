127b48036298544e5c1352f4decbfdc5e14612b3
Cyfroni <karolplg@gmail.com>
2019-04-07 21:15:35 +0200

    refactor

--- Chapel/compile.sh
+++ Chapel/compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ../chapel-1.18.0/util/setchplenv.bash
-chpl Source2.chpl -o chpl.out
+chpl Source.chpl -o chpl.out
--- OpenMP/compile.sh
+++ OpenMP/compile.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-g++ -std=c++11 Source_.cpp -o openmp.out -fopenmp
+g++ -std=c++11 Source.cpp -o openmp.out -fopenmp
