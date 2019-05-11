a1c6b76521d8a8056a96449967a592335663c930
Karol <karolplg@gmail.com>
2019-05-11 04:59:06 +0200

    reformat

--- OpenMP/Source.cpp
+++ OpenMP/Source.cpp
@@ -94,28 +94,28 @@ void generic(vec &dp, const vector <array<int, 3>> &g, const int s) {
         dp[x] = make_pair(INT_MAX / 2, -1);       // initialize with big distance
     }                                             // and no predecessor
 
-    dp[s].first = 0;                      // distance from source to source is 0
+    dp[s].first = 0;                              // distance from source to source is 0
     deque<int> V;
     vector<bool> is(_n, false);                   // table of existance in V
-    V.push_back(s);                      // Initialize queue with source node
-    is[s] = true;                            // mark it in the table
+    V.push_back(s);                               // Initialize queue with source node
+    is[s] = true;                                 // mark it in the table
 
     while (!V.empty()) {
-        int _i = V.front();                         // take from the top
+        int _i = V.front();                       // take from the top
         V.pop_front();
-        is[_i] = false;                        // mark it
+        is[_i] = false;                           // mark it
 
         for (const auto &e : g) {
             const auto i = e[0] - 1;
             const auto j = e[1] - 1;
             const auto a = e[2];
-            if (i != _i || j==s) continue;
-
-            if (dp[i].first + a < dp[j].first) {
-                dp[j].first = dp[i].first + a;
-                dp[j].second = i;
+            if (i != _i || j==s) continue;        // process only edges from i
+                                                  // and ommit edges to source
+            if (dp[i].first + a < dp[j].first) {  // relaxation
+                dp[j].first = dp[i].first + a;    // update distance
+                dp[j].second = i;                 // update predecessor
                 if (!is[j]) {
-                    V.push_back(j);
+                    V.push_back(j);               // add v to V if it's not there already.
                     is[j] = true;
                 }
             }
@@ -123,44 +123,44 @@ void generic(vec &dp, const vector <array<int, 3>> &g, const int s) {
     }
 }
 
-void slf(vec &table, const vector <array<int, 3>> &g, const int source) {
+void slf(vec &dp, const vector <array<int, 3>> &g, const int s) {
 // dp - vector of distances and predecessors (d, p)
 // g - graph (i, j, a)
 // s - start node
 
-    for (auto i = 0; i < _n; ++i) {
-        table[i] = make_pair(INT_MAX / 2, -1);
-    }
+    for (auto x = 0; x < _n; ++x) {
+        dp[x] = make_pair(INT_MAX / 2, -1);       // initialize with big distance
+    }                                             // and no predecessor
 
-    table[source].first = 0;
-    deque<int> Q;
-    vector<bool> is(_n, false);
-    Q.push_back(source + 1);
-    is[source] = true;
+    dp[s].first = 0;                              // distance from source to source is 0
+    deque<int> V;
+    vector<bool> is(_n, false);                   // table of existance in V
+    V.push_back(s);                               // Initialize queue with source node
+    is[s] = true;                                 // mark it in the table
 
-    while (!Q.empty()) {
-        int u = Q.back();
-        Q.pop_back();
-        is[u - 1] = false;
-        int v;
-        for (const auto &j : g) {
-            if (u == j[0])
-                v = j[1];
-            else if (u == j[1])
-                v = j[0];
-            else continue;
-            const auto u1 = u - 1;
-            const auto v1 = v - 1;
-            const auto w = j[2];
-            if (table[u1].first + w < table[v1].first) {
-                table[v1].first = table[u1].first + w;
-                table[v1].second = u;
-                if (!is[v1]) {
-                    if (!Q.empty() && table[v1].first < Q.back())
-                        Q.push_front(v);
-                    else
-                        Q.push_back(v);
-                    is[v1] = true;
+    while (!V.empty()) {
+        int _i = V.front();                       // take from the top
+        V.pop_front();
+        is[_i] = false;                           // mark it
+
+        for (const auto &e : g) {
+            const auto i = e[0] - 1;
+            const auto j = e[1] - 1;
+            const auto a = e[2];
+            if (i != _i || j==s) continue;        // process only edges from i
+                                                  // and ommit edges to source
+            if (dp[i].first + a < dp[j].first) {  // relaxation
+                dp[j].first = dp[i].first + a;    // update distance
+                dp[j].second = i;                 // update predecessor
+                if (!is[j]) {
+                    if (!V.empty() &&             // SLF rule for insertion
+                        dp[j].first <= dp[Q.front()].first){
+                        V.push_front(j);
+                    }
+                    else {
+                        V.push_back(j);
+                    }
+                    is[j] = true;
                 }
             }
         }
